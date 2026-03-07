2b9954085984a150f692b21aa955bfd13a0fc98a
Alvaro Garayar <alvarogarayar@Alvaros-Laptop.local>
2026-03-07 21:49:26 +0700

    style: change services banner background image to bannerd.jpg

--- components/ServicesBanner.tsx
+++ components/ServicesBanner.tsx
@@ -83,7 +83,7 @@ const ServicesBanner: React.FC = () => {
         >
             {/* Background image */}
             <img
-                src="/bannerp.jpg"
+                src="/bannerd.jpg"
                 alt="Fondo banner"
                 style={{
                     position: 'absolute', inset: 0,
