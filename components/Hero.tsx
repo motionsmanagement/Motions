2a72e922beb382183d41a7ed78bedae49424b9d7
Alvaro Garayar <alvarogarayar@Alvaros-Laptop.local>
2026-03-23 17:15:40 +0700

    style: mover 'digital' a la segunda linea en desktop y ajuste de tamaño movil

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -36,9 +36,9 @@ const Hero: React.FC = () => {
           </h1>
 
           <div className="mb-10 w-full">
-            <p className="text-white/80 text-[14px] sm:text-base md:text-xl lg:text-[1.35rem] max-w-3xl mx-auto md:mx-0 leading-tight md:leading-relaxed tracking-tight">
-              Dedicados al sector gastronómico, mejoramos tu <br /> 
-              presencia digital y convertimos visitas en reservas.
+            <p className="text-white/80 text-[13.5px] sm:text-base md:text-xl lg:text-[1.35rem] max-w-3xl mx-auto md:mx-0 leading-tight md:leading-relaxed tracking-tight">
+              Dedicados al sector gastronómico, mejoramos tu presencia <br /> 
+              digital y convertimos visitas en reservas.
             </p>
           </div>
 
