3260809a279fc3794ca85fe9edc53fbf31ef7e7c
Alvaro Garayar <alvarogarayar@Alvaros-Laptop.local>
2026-04-03 16:42:54 +0700

    style: unificar fondo de AdBanner con 'motionsweb.jpg' para todos los dispositivos

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -28,7 +28,7 @@ const AdBanner: React.FC = () => {
                 {/* Mobile Version */}
                 <div className="md:hidden w-full">
                     <img 
-                        src="/adbannermobilemotions.jpg" 
+                        src="/motionsweb.jpg" 
                         alt="Publicidad Motions Mobile" 
                         className="w-full h-auto object-cover"
                     />
